a2629edaf5de2831c0d08e747c320c1f4e27f6c7
pegchose <j.c.lsevp9@gmail.com>
2019-05-19 15:18:59 +0900

    データベース設計

--- db/migrate/20190515093744_create_messages.rb
+++ db/migrate/20190515093744_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
       t.text        :body, null:false
       t.string      :image
+      t.integer     :message_id
       t.integer     :group_id
       t.integer     :user_id
       t.timestamps
--- db/migrate/20190515100432_create_users.rb
+++ db/migrate/20190515100432_create_users.rb
@@ -1,11 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.string      :name, null:false
+      t.integer     :user_id 
+      # t.text        :body null:false  <= user_idとmessage_idで判断すれば良い
+      t.integer     :group_id
       t.timestamps
     end
 
-    add_column :users, :name, :string
+    add_index :users, :name, :string
     
   end
 end
